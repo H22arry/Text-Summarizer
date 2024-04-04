3d84bbed4978ca0199df781e050c0de11f6905b1
abhishek mutkekar <abhishekmutkekar@kccemsr.edu.in>
2024-04-04 17:08:44 +0530

    Front End HTML

--- app.py
+++ app.py
@@ -1,32 +1,35 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 import requests
+from flask import request as req 
 
 app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def Index():
-    API_URL = "https://api-inference.huggingface.co/models/sshleifer/distilbart-cnn-12-6"
-    headers = {"Authorization": "Bearer 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"}
+    return render_template("index.html")
 
-    data = '''The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, 
-		    and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction,
-            the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. 
-            It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). 
-            Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct.'''
+@app.route("/Summarize",methods=["GET","POST"])
+def Summarize():
+    if req.method=="POST":
+        API_URL = "https://api-inference.huggingface.co/models/sshleifer/distilbart-cnn-12-6"
+        headers = {"Authorization": "Bearer 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"}
 
-    minl= 30#int(input()) # for taking user input
-    maxl= 70#int(input())
-    def query(payload):
-	    response = requests.post(API_URL, headers=headers, json=payload)
-	    return response.json()
+        data = req.form["data"]
+
+        minl= 30#int(input()) # for taking user input
+        maxl= int(req.form["maxl"])#int(input())
+        def query(payload):
+	        response = requests.post(API_URL, headers=headers, json=payload)
+	        return response.json()
     
-    output = query({
-	    "inputs": data,
-	    "parameters": {"min_length":minl,"max_length":maxl},
-	    })
+        output = query({
+	        "inputs": data,
+	        "parameters": {"min_length":minl,"max_length":maxl},
+	    })[0]
 
+        return render_template("index.html",result=output["summary_text"])
 
-    return render_template("index.html",result=output,mini=minl)
-
+    else:
+         return render_template("index.html")
 
 if __name__=='__main__':
     app.debug = True
